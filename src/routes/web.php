b520d1b9f11ce30ac61950f4d77395036a6147d9
Vinicius <vicmen33@hotmail.com>
2019-07-24 01:56:02 -0400

    Criação do package

---
+++ src/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// MyVendor\contactform\src\routes\web.php
+use Illuminate\Support\Facades\Route;
+
+Route::get('test', function () {
+    return 'Hello from the contact form package';
+});
